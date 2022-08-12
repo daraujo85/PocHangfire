7ba64747e39659f9e3bc5c56b6c1ecebff181c58
Diego Araujo <jcresgate@gmail.com>
2022-08-12 00:19:53 -0300

    Add files via upload

---
+++ Controllers/JobsController.cs
@@ -0,0 +1,60 @@
+﻿using System;
+using Hangfire;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.Logging;
+
+namespace PocOptimusHangfire.Controllers
+{
+  [ApiController]
+  [Route("api/[controller]")]
+  public class JobsController : ControllerBase
+  {
+    private readonly ILogger<JobsController> _logger;
+
+    public JobsController(ILogger<JobsController> logger)
+    {
+      _logger = logger;
+    }
+
+    [HttpPost]
+    [Route("ImediatoSucesso")]
+    public IActionResult ImediatoSucesso()
+    {
+      var jobId = BackgroundJob.Enqueue(() => Service.DelaySuccess());
+
+      return Ok(new { jobId = jobId });
+    }
+    [HttpPost]
+    [Route("ImediatoFalha")]
+    public IActionResult ImediatoFalha()
+    {
+      var jobId = BackgroundJob.Enqueue(() => Service.DelayFail());
+
+      return Ok(new { jobId = jobId });
+    }
+    [HttpPost]
+    [Route("DiarioSucesso")]
+    public IActionResult DiarioSucesso()
+    {
+      var jobId = BackgroundJob.Enqueue(() => Service.DailySuccess());
+
+      return Ok(new { jobId = jobId });
+    }
+    [HttpPost]
+    [Route("InicioAtrasado")]
+    public IActionResult InicioAtrasado()
+    {
+      var jobId = BackgroundJob.Schedule(() => Service.Delayed(),TimeSpan.FromMinutes(1));
+      return Ok();
+    }
+    [HttpPost]
+    [Route("Cascata/{jobId}")]
+    [Obsolete]
+    public IActionResult Cascata(string jobId)
+    {
+      var jobIdResult = BackgroundJob.ContinueWith(jobId,() => Console.WriteLine("Continuando e executando o servico B!"));
+      
+      return Ok(jobIdResult);
+    }
+  }
+}
---
+++ Service.cs
@@ -0,0 +1,42 @@
+using System;
+using System.Threading;
+using Hangfire;
+
+public static class Service
+{
+  public static void DelaySuccess()
+  {
+    Console.WriteLine("Etapa 1");
+    Thread.Sleep(5000);
+    Console.WriteLine("Etapa 2");
+    Thread.Sleep(2000);
+    Console.WriteLine("Ultima Etapa");
+  }
+  [AutomaticRetry(Attempts = 3)]
+  public static void DelayFail()
+  {
+    try
+    {
+      Console.WriteLine("Etapa 1");
+      Thread.Sleep(1000);
+      Console.WriteLine("Etapa 2");
+      Thread.Sleep(2000);
+      Convert.ToInt32("Erro de conversao de inteiro");
+    }
+    catch (Exception ex)
+    {
+      Console.WriteLine(ex.Message);
+    }
+
+  }
+  public static void DailySuccess()
+  {
+    RecurringJob.AddOrUpdate(
+    () => Console.WriteLine("Diário recorrente!"),
+    Cron.Daily);
+  }
+  public static void Delayed()
+  {
+    Console.WriteLine("Delayed!");
+  }
+}
